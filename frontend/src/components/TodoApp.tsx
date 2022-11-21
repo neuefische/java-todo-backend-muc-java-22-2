a4a293e3f984de957178861c7f133093cab55d09
Sergej Grilborzer <sergej@grilborzer.de>
2022-11-21 11:46:39 +0100

    Create TodoList & GET todo request function

--- frontend/src/components/TodoApp.tsx
+++ frontend/src/components/TodoApp.tsx
@@ -1,12 +1,41 @@
-
 // export default = Standardexport
+import TodoList from "./TodoList";
+import axios from "axios";
+import {Todo} from "../models/Todo";
+import {useState} from "react";
+
 export default function TodoApp() {
 
+    const todoBaseUrl = "/api/todo";
+    // Was ist ein State?
+    // React Mechanismus, um Daten zu aktualisieren,
+    // wenn diese sich ändern, wird neu gerendert
+
+    // todos = Daten
+    // setTodos = Funktionen um die Daten zu ändern
+    const [todos, setTodos] = useState<Todo []>([])
+
+    function getTodos() {
+        // get weil HTTP GET
+        // Eine Anfrage ist asynchron
+        // Um auf die Antwort zu antworten SOBALD sie da ist,
+        // arbeiten wir mit "Promises"
+        axios.get(todoBaseUrl)
+            // Was passiert, wenn die Anfrage erfolgreich war
+            .then(todoListResponse => {
+                const newTodoList: Todo[] = todoListResponse.data;
+                setTodos(newTodoList)
+            })
+            // ... und wenn's schief läuft...?
+            .catch(errorResponse => {
+                console.log("ALARM! Es gab einen Fehler beim GET: " + errorResponse)
+            })
+    }
 
     return (
-     <section>
-         <h1>Beste Todo App wo geht</h1>
-
-     </section>
+        <section>
+            <h1>Beste Todo App wo geht</h1>
+            <TodoList todosToMap={todos}/>
+        </section>
     )
 }
---
+++ frontend/src/components/TodoList.tsx
@@ -0,0 +1,21 @@
+// Komponentename + Props
+import {Todo} from "../models/Todo";
+import TodoItem from "./TodoItem";
+
+type TodoListProps = {
+    todosToMap: Todo []
+}
+
+// Erstellt eine Liste von den einzelnen Todo Komponenten
+export default function TodoList(props: TodoListProps ) {
+
+    const todoItemComponents = props.todosToMap.map(todoData => {
+        return <TodoItem todoToDisplay={todoData} />
+    })
+
+    return (
+        <section>
+            {todoItemComponents}
+        </section>
+    )
+}
